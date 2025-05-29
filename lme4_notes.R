825183bdee5a943e65108d5f65309c75cdb9e5fb
Ben Bolker <bbolker@gmail.com>
2025-05-29 19:09:54 -0400

    notes on formula processing

---
+++ lme4_notes.R
@@ -0,0 +1,10 @@
+##
+library(reformulas)
+## from misc/ex_flexLambda.Rmd in flexLambda brancha
+## form <- y ~ time + ar1d(~(time|id))
+form <- y ~ time + ar1(time|id)
+fr <- expand.grid(time = 1:10, id = factor(1:10))
+findbars_x(form)
+tt <- mkReTrms(findbars(form), fr)
+
+splitForm(form, specials = c(names(glmmTMB:::.valid_covstruct), "s"))
